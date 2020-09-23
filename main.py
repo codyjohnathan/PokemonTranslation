bafe5dde56bbf5459f2c635c8d39c1d6e4302705
codyjohnathan <70065126+codyjohnathan@users.noreply.github.com>
2020-09-23 22:51:45 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,48 +8,51 @@ api = Api(app)
 
 class Pokemon(Resource):
     def get(self, name, description):
-        return info[name, description]
+        return info[name, description], 200
 
 
 # Prints all Pokemon via JSON
-
-@app.route('/api/v1/pokemon/all')
+@app.route('/v1/pokemon/all', methods=['GET'])
 def poke_names():
     data = []
-    url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+    name_url = "https://pokeapi.co/api/v2/pokemon?limit=151"
     while True:
-        resp = requests.get(url)
+        resp = requests.get(name_url)
         json = resp.json()
         data.extend(json.get('results', []))
-        url = json.get('next')
-        if not url: break
+        name_url = json.get('next')
+        if not name_url: break
     return jsonify(data)
 
-@app.route('/api/v1/pokemon/<string:name>')
-def get_poke(name):                  
+#flavor Text, needs sorting
+@app.route('/v1/pokemon/<int:pokemon_id>', methods=['GET'])
+def get_description(pokemon_id):
+    descrip_url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}"
+    r = requests.get(descrip_url)
+    json_blob = r.json()
+    flav_text = extract_descriptive_text(json_blob)
+    return jsonify({'description': flav_text})
+
+
+def extract_descriptive_text(json_blob, language='en'):
+    text = []
+    for f in json_blob['flavor_text_entries']:
+        if f['language']['name'] == language:
+            text.append(f['flavor_text'])
+    return text
+
+#attempting to remove \n from json(flavor)
+def deletecrap():
+     for i in json_blob['flavor_text_entries']:
+         if i == "\n":
+             json_blob.remove("\n")
+     return json_blob
+
+
+@app.route('/v1/pokemon/<string:name>', methods=['GET'])
+def get_poke(name):
      return jsonify({'name': name})
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-#     Useful code, do not discard, FROM POSTMAN
-# payload = "{\n    \"name\": \"Julian\",\n    \"message\": \"Posting JSON data to Flask!\"\n}"
-# headers = {
-#   'Content-Type': 'text/plain',
-#   'Cookie': '__cfduid=d819fa7205412e277649e0ce70eb381211600699952'
-# }
-
-
-# @app.route('/api/v1/pokemon/all')
-# def poke_names():
-#     to_parse =response.text.encode('utf8')
-#     response_data = {name:"name", descrip:"description"}
-#     return jsonify(**response_data)
-
-# @app.route('/api/v1/pokemon/<string:name>', methods=['GET'])
-# def get_poke(name):
-#     return jsonify({'name': name})
-
-
-# https://pokeapi.co/api/v2/pokemon?limit=151%27    original 151 Pokemon
