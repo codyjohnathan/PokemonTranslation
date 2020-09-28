388695dbb97b66705ca5decf2566123d2b927430
codyjohnathan <70065126+codyjohnathan@users.noreply.github.com>
2020-09-28 18:12:15 +0100

    Add files via upload
    One endpoint for PokeAPI data

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from flask import Flask, jsonify, Response
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, fields, marshal_with
 import requests, json
 
 app = Flask(__name__)
 api = Api(app)
 
-
-class Pokemon(Resource):
-    def get(self, name, description):
-        return info[name, description], 200
-
-
-# Prints all Pokemon via JSON
-@app.route('/v1/pokemon/all', methods=['GET'])
+# Returns all Pokemon via JSON
+@app.route('/v1/pokemon/', methods=['GET'])
 def poke_names():
     data = []
     name_url = "https://pokeapi.co/api/v2/pokemon?limit=151"
@@ -24,15 +18,6 @@ def poke_names():
         if not name_url: break
     return jsonify(data)
 
-#flavor Text, needs sorting
-@app.route('/v1/pokemon/<int:pokemon_id>', methods=['GET'])
-def get_description(pokemon_id):
-    descrip_url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}"
-    r = requests.get(descrip_url)
-    json_blob = r.json()
-    flav_text = extract_descriptive_text(json_blob)
-    return jsonify({'description': flav_text})
-
 
 def extract_descriptive_text(json_blob, language='en'):
     text = []
@@ -41,18 +26,30 @@ def extract_descriptive_text(json_blob, language='en'):
             text.append(f['flavor_text'])
     return text
 
-#attempting to remove \n from json(flavor)
-def deletecrap():
-     for i in json_blob['flavor_text_entries']:
-         if i == "\n":
-             json_blob.remove("\n")
-     return json_blob
 
-
-@app.route('/v1/pokemon/<string:name>', methods=['GET'])
+@app.route('/v1/pokemon/<string:name>')
 def get_poke(name):
-     return jsonify({'name': name})
+    descrip_url = f"https://pokeapi.co/api/v2/pokemon-species/{name}"
+    r = requests.get(descrip_url)
+    json_blob = r.json()
+    flav_text = extract_descriptive_text(json_blob)
+    return jsonify({'name': name},{'description': flav_text})
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+#returns Pokemon names
+# @app.route('/v1/pokemon/<string:name>/title', methods=['GET'])
+# def get_poke(name):
+#      return jsonify({'name': name})
+
+
+#flavor Text ie pokemon description
+# @app.route('/v1/pokemon/<int:pokemon_id>', methods=['GET'])
+# def get_description(pokemon_id):
+#     descrip_url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}"
+#     r = requests.get(descrip_url)
+#     json_blob = r.json()
+#     flav_text = extract_descriptive_text(json_blob)
+#     return jsonify({'description': flav_text})
