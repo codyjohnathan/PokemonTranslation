3d196dfef86d0315f39c67b7e37aa4fa07483bf2
codyjohnathan <johnathan.cody1992@gmail.com>
2020-09-30 21:21:28 +0100

    Test for Pycharm integration with Github account Cleared some unused package imports

--- main.py
+++ main.py
@@ -1,47 +1,14 @@
-from flask import Flask, jsonify
-from flask_restful import Api
-import requests
-from flask_caching import Cache
+from flask import Flask, jsonify, Response
+from flask_restful import Resource, Api
+import requests, json
 
 app = Flask(__name__)
 api = Api(app)
-
-cache = Cache()
-
-
-app.config["JSON_SORT_KEYS"] = False #By default Flask will serialize JSON objects in a way that the keys are ordered, this overrides this behavior
-app.config = {
-     "CACHE_TYPE": "simple",
-     "CACHE_DEFAULT_TIMEOUT": 5
-    }
-
-cache.init_app(app)
-
-
-def extract_descriptive_text(json_blob, language='en', version= 'red'):
-    """
-    Parses through nested dictionary from Poke API and grabs all flavor text entries
-    that are in english and come from the latest Pokemon game, Sword, in order to
-    get the most up to date info
-    :param json_blob:
-    :param language:
-    :param version:
-    :return:
-    """
-    text = ""
-    for f in json_blob['flavor_text_entries']:
-        if f['language']['name'] == language and f['version']['name'] == version:
-            text = f['flavor_text']
-    return text
 
 
 # Returns first 150 Pokemon via JSON
 @app.route('/pokemon/', methods=['GET'])
 def poke_names():
-    """
-
-    :return:
-    """
     data = []
     name_url = "https://pokeapi.co/api/v2/pokemon?limit=151"
     while True:
@@ -49,46 +16,41 @@ def poke_names():
         json = resp.json()
         data.extend(json.get('results', []))
         name_url = json.get('next')
-        if not name_url:
-            break
+        if not name_url: break
     return jsonify(data)
 
 
-#sending Pokemon junk to be translated and returned
-@app.route('/pokemon/<string:name>/', methods=['GET', 'POST'])
-def get_translation(name):
-    """
+def extract_descriptive_text(json_blob, language='en'):
+    text = []
+    for f in json_blob['flavor_text_entries']:
+        if f['language']['name'] == language:
+            text.append(f['flavor_text'])
+    return text
 
-    :param name:
-    :return:
-    """
-    descrip_url = f"https://pokeapi.co/api/v2/pokemon-species/{name.lower()}"
+
+@app.route('/pokemon/<string:name>')
+def get_poke(name):
+    descrip_url = f"https://pokeapi.co/api/v2/pokemon-species/{name}"
     r = requests.get(descrip_url)
     json_blob = r.json()
-    text_trans = extract_descriptive_text(json_blob)
-    clean_description = text_trans.replace("\n", " ")
-    trans_url = f"https://api.funtranslations.com/translate/shakespeare.json?text={clean_description}"
-    shakespeare = requests.get(trans_url)
-    try:
-        useful_info = shakespeare.json()['contents']['translated']
-    except KeyError:
-        useful_info = "Looks like you've exceeded server limit of 5 requests an hour!"
-    return jsonify({'name': name, 'description': useful_info})
+    flav_text = extract_descriptive_text(json_blob)
+    return jsonify({'name': name},{'description': flav_text})
 
 
-if __name__ == '__main__':
-    app.run(debug=False) #was True during development
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#returns Pokemon names
+# @app.route('/v1/pokemon/<string:name>/title', methods=['GET'])
+# def get_poke(name):
+#      return jsonify({'name': name})
 
 
-# Old method for taking correct contents from translation API
-# def extract_useful_info(translated):
-#     """
-#
-#     :param translated:
-#     :return:
-#     """
-#     text = ""
-#     wanted_info = translated['contents']['translated']
-#     text += wanted_info
-#     return text
-
+#flavor Text ie pokemon description
+# @app.route('/v1/pokemon/<int:pokemon_id>', methods=['GET'])
+# def get_description(pokemon_id):
+#     descrip_url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}"
+#     r = requests.get(descrip_url)
+#     json_blob = r.json()
+#     flav_text = extract_descriptive_text(json_blob)
+#     return jsonify({'description': flav_text})
