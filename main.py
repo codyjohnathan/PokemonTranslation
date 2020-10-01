591fe9e8188257be5e034d6f677baade3e785c99
codyjohnathan <johnathan.cody1992@gmail.com>
2020-10-01 15:47:47 +0100

    Started to integrate the Fun TRanslations API with my pokemon Flavor-Text description

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ api = Api(app)
  
 
 # Returns first 150 Pokemon via JSON
-@app.route('/v1/pokemon/', methods=['GET'])
+@app.route('/pokemon/', methods=['GET'])
 def poke_names():
     data = []
     name_url = "https://pokeapi.co/api/v2/pokemon?limit=151"
@@ -20,24 +20,38 @@ def poke_names():
     return jsonify(data)
 
 
-def extract_descriptive_text(json_blob, language='en'):
+def extract_descriptive_text(json_blob, language='en', version="sword"):
     text = []
     for f in json_blob['flavor_text_entries']:
-        if f['language']['name'] == language:
+        if f['language']['name'] == language and f['version']['name'] == version: #searches through nested requests for version entry to specifiy to grab information only from red
             text.append(f['flavor_text'])
     return text
+ # and  f['version']['name'] == version
 
 
-@app.route('/v1/pokemon/<string:name>')
+#original pokemon name and DESCRIPTION
+@app.route('/pokemon/original/<string:name>/', methods=['GET'])
 def get_poke(name):
     descrip_url = f"https://pokeapi.co/api/v2/pokemon-species/{name}"
     r = requests.get(descrip_url)
     json_blob = r.json()
     flav_text = extract_descriptive_text(json_blob)
-    return jsonify({'name': name},{'description': flav_text})
+    return jsonify({'name':name}, {'description': flav_text})
+
+#sending Pokemon junk to be translated and returned
+@app.route('/pokemon/<string:name>/', methods=['GET'])
+def get_translation(name):
+    descrip_url = f"https://pokeapi.co/api/v2/pokemon-species/{name}"
+    r = requests.get(descrip_url)
+    json_blob = r.json()
+    text_trans = extract_descriptive_text(json_blob)
+    trans_url = f"https://api.funtranslations.com/translate/shakespeare.json?text={text_trans}"
+    shakespeare = requests.get(trans_url)
+    translated = shakespeare.json()
+    return jsonify({'name': name}, {'description': translated})
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
 
 #returns Pokemon names
@@ -54,3 +68,11 @@ if __name__ == "__main__":
 #     json_blob = r.json()
 #     flav_text = extract_descriptive_text(json_blob)
 #     return jsonify({'description': flav_text})
+
+
+# def extract_descriptive_text(json_blob, language='en'):
+#     text = []
+#     for f in json_blob['flavor_text_entries']:
+#         if f['language']['name'] == language: #discerns what to append to our list by the criteria that it's in English
+#             text.append(f['flavor_text'])
+#     return text
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests #GET and PUT requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "/api/v1/pokemon/all" )
+response = requests.get(BASE + "/pokemon/charizard" )
 print(response.json())
