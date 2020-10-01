65d5b1b84de626c8b7b1d9d97712681360c71069
codyjohnathan <johnathan.cody1992@gmail.com>
2020-10-01 07:32:30 +0100

    Edited URL resource

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ api = Api(app)
  
 
 # Returns first 150 Pokemon via JSON
-@app.route('/v1/pokemon/', methods=['GET'])
+@app.route('/pokemon/', methods=['GET'])
 def poke_names():
     data = []
     name_url = "https://pokeapi.co/api/v2/pokemon?limit=151"
@@ -20,21 +20,21 @@ def poke_names():
     return jsonify(data)
 
 
-def extract_descriptive_text(json_blob, language='en'):
+def extract_descriptive_text(json_blob, language='en', version="sword"):
     text = []
     for f in json_blob['flavor_text_entries']:
-        if f['language']['name'] == language:
+        if f['language']['name'] == language and f['version']['name'] == version: #searches through nested requests for version entry to specifiy to grab information only from red
             text.append(f['flavor_text'])
     return text
+ # and  f['version']['name'] == version
 
-
-@app.route('/v1/pokemon/<string:name>')
+@app.route('/pokemon/<string:name>')
 def get_poke(name):
     descrip_url = f"https://pokeapi.co/api/v2/pokemon-species/{name}"
     r = requests.get(descrip_url)
     json_blob = r.json()
     flav_text = extract_descriptive_text(json_blob)
-    return jsonify({'name': name},{'description': flav_text})
+    return jsonify({'name':name}, {'description': flav_text})
 
 
 if __name__ == "__main__":
@@ -54,3 +54,11 @@ if __name__ == "__main__":
 #     json_blob = r.json()
 #     flav_text = extract_descriptive_text(json_blob)
 #     return jsonify({'description': flav_text})
+
+
+# def extract_descriptive_text(json_blob, language='en'):
+#     text = []
+#     for f in json_blob['flavor_text_entries']:
+#         if f['language']['name'] == language: #discerns what to append to our list by the criteria that it's in English
+#             text.append(f['flavor_text'])
+#     return text
