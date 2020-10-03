7a879e491b7766f4edfccf427ac4108409b97f74
codyjohnathan <johnathan.cody1992@gmail.com>
2020-10-03 20:23:12 +0100

    Started to integrate the Fun TRanslations API with my pokemon Flavor-Text description

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests #GET and PUT requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "/pokemon/mewtwo" )
+response = requests.get(BASE + "/pokemon/charizard" )
 print(response.json())
